7bde925a7238cc8262f255c2957e583e8fd97587
bbabcock88 <33706289+bbabcock88@users.noreply.github.com>
2022-07-06 23:05:20 -0500

    Update jumps.py
    added comments to main loop

--- jumps.py
+++ jumps.py
@@ -19,35 +19,46 @@ for x in cons[start]:
 copy2 = paths[:]
 
 while complete == 0:
+    ## copy of paths for lower loops to ref
     copy2 = paths[:]
     counter1 = 0
     for x in paths:
+        ## looking at next system from 1 starting path
         temp = x[-1]
         counter2 = 0
+        ## copy path to add into if next jump has value
         copy = x[:]
+        ## delete old path
         del(copy2[0])
         counter1 = counter1 + 1
         for y in cons[temp]:
             nexthop = []
             temp2 = y[1]
+            ## check if system hop is final dest
             if temp2 == end:
                 print('complete')
                 print(copy + [temp2])
                 print('Jumps '+str(len(copy + [temp2])-1))
                 complete = 1
                 break
+            ## get current path length, check if next system has already been visted by another path
             pathlen = len(x)
             if temp2 in dijkstra.keys():
+                ## check if this path is faster to this system if not then no path added back to paths, if so add system and jumps to dict
                 if dijkstra[temp2] > pathlen:
                     dijkstra[temp2] = pathlen
                     nexthop = copy[:]
                     nexthop = nexthop + [temp2]
+                    ## add new path with this jump in it to paths copy
                     copy2 = copy2 + [nexthop]
                     counter2 = counter2 + 1
             else:
+                ## adding system and jumps to dict
                 dijkstra[temp2] = pathlen
                 nexthop = copy[:]
                 nexthop = nexthop + [temp2]
+                ## add new path with this jump in it to paths copy
                 copy2 = copy2 + [nexthop]
                 counter2 = counter2 + 1
+    ## once this round of loops is done add all new path into paths and start the while loop over
     paths = copy2[:]
